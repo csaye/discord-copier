aa5facf5a4bb49ee8fb80e371be743d89d60cfab
Cooper Saye <coopersaye@gmail.com>
2021-06-30 15:34:35 -0700

    Send message on connect.

--- bot.py
+++ bot.py
@@ -7,4 +7,12 @@ tokenfile.close()
 
 client = discord.Client()
 
+@client.event
+async def on_ready():
+    print(f'${client.user} has connected to Discord')
+    for guild in client.guilds:
+        if not guild.channels: continue # if no valid channel, continue
+        channel = guild.channels[1] # get first channel
+        await channel.send('hello world')
+
 client.run(token)
