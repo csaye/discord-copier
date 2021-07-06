80bb7a16add811f89f31b45ee6fa81727e879b69
Cooper Saye <coopersaye@gmail.com>
2021-07-06 16:31:54 -0700

    Check for new messages from file.

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import time, threading, asyncio
 from discord.ext import commands
 
 # read token from file
@@ -6,6 +7,23 @@ token = tokenfile.read()
 tokenfile.close()
 
 bot = commands.Bot(command_prefix='!')
+context = None
+last_timestamp = None
+
+# checks for message to send
+async def check_message():
+    global last_timestamp, context
+    # read message from file
+    f = open('./message.txt')
+    text = f.read()
+    if not text: return
+    timestamp, message = text.splitlines()
+    f.close()
+    # if new message, send to context
+    if timestamp != last_timestamp and context:
+        print(f'sending message {message}')
+        last_timestamp = timestamp
+        await context.send(message)
 
 @bot.event
 async def on_ready():
