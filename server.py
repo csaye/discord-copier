92de9a89668b88d879b55932b2f120ab45d307d5
Cooper Saye <coopersaye@gmail.com>
2021-07-06 16:31:07 -0700

    Rewrite server with websockets.

--- server.py
+++ server.py
@@ -1,15 +1,14 @@
-import socketserver
+import asyncio, websockets, datetime
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        self.request.sendall(self.data.upper())
+async def handler(websocket, path):
+    message = await websocket.recv()
+    print(f'received message {message}')
+    f = open('./message.txt', 'w')
+    timestamp = datetime.datetime.now()
+    f.write(f'{timestamp}\n{message}')
+    f.close()
 
-# set up socket server
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        print(f'socket server started on {HOST}:{PORT}')
-        server.serve_forever()
+start_server = websockets.serve(handler, "localhost", 9999)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
