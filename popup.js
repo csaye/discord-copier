97b7c3103f59d983a6ddd3f80af25b8d8a6f2984
Cooper Saye <coopersaye@gmail.com>
2021-07-08 14:11:18 -0700

    Improve message styling.

--- popup.js
+++ popup.js
@@ -17,13 +17,26 @@ startButton.addEventListener("click", async () => {
 
 // will be executed after copy starts
 function startCopy() {
+  let lastText = undefined;
+
   // will be called when messages change
   function onMutation(mutations) {
     for (const mutation of mutations) {
       for (const addedNode of mutation.addedNodes) {
+        // skip if doubled message
+        if (addedNode.textContent === lastText) continue;
+        else lastText = addedNode.textContent;
+        // get styled message content
+        const content = addedNode.children[0].children;
+        if (!content.length) continue;
+        const message = content.length === 2 ?
+        `**${content[0]?.textContent?.trim()}** ${content[1]?.textContent}` :
+        `**${content[1]?.textContent?.trim()}** ${content[2]?.textContent}`;
+        console.log(message);
         // create new connection and send content
         const connection = new WebSocket("ws://localhost:9999");
-        connection.onopen = e => connection.send(addedNode.textContent);
+        connection.onopen = e => connection.send(message);
+        connection.onerror = e => alert('WebSocket connection failed. Make sure you have started the server.');
       }
     }
   }
